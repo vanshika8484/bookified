b4410decdea21baf959354136dc680443111c7d9
vanshika8484 <vanshikaagarwal8484@gmail.com>
2026-03-27 14:56:18 +0530

    create homepage UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
+import BookCard from '@/components/BookCard'
 import HeroSection from '@/components/HeroSection'
+import { sampleBooks } from '@/lib/constants'
 import React from 'react'
 
 const Page = () => {
     return (
         <div>
             <HeroSection />
+            <div className='library-books-grid'>
+                {sampleBooks.map((book) => (
+                    <BookCard key={book._id} title={book.title} author={book.author} coverURL={book.coverURL} slug={book.slug} />
+                ))}
+            </div>
         </div>
     )
 }
